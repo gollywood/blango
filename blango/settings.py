3dd39a41abfcd892f723491f79a809babf43a5e3
gollywood <5h4dyD12!0402>
2022-02-18 12:07:14 +0100

    "Finish throttling"

--- blango/settings.py
+++ blango/settings.py
@@ -192,8 +192,21 @@ class Dev(Configuration):
             "rest_framework.authentication.TokenAuthentication",
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
-        ]
+        ],
+        "DEFAULT_THROTTLE_CLASSES": [
+            "blog.api.throttling.AnonSustainedThrottle",
+            "blog.api.throttling.AnonBurstThrottle",
+            "blog.api.throttling.UserSustainedThrottle",
+            "blog.api.throttling.UserBurstThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {
+            "anon_sustained": "500/day",
+            "anon_burst": "10/minute",
+            "user_sustained": "5000/day",
+            "user_burst": "100/minute",
+        },
     }
+
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
